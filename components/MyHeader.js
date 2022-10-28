8a9291f7bc9638ec38d865aa35418653bd8fe911
MSAZ89 <michaelsimonaz@gmail.com>
2022-10-28 00:35:02 -0700

    routing

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -2,24 +2,27 @@ import NextLink from "next/link"
 import { Heading, Stack, Button } from '@chakra-ui/react'
 
 export default function MyHeader(props) {
+
+    let scheme = "green";
+
     return (
         <header>
             <nav>
                 <Stack direction={['column', 'row']} spacing={4} align='center'>
                     <NextLink href='/' passHref>
-                        <Button colorScheme='blue' variant='outline'>Home</Button>
+                        <Button colorScheme={scheme} variant='outline'>Home</Button>
                     </NextLink>
                     <NextLink href='/about' passHref>
-                        <Button colorScheme='blue' variant='outline'>About</Button>
+                        <Button colorScheme={scheme} variant='outline'>About</Button>
                     </NextLink>
                     <NextLink href='/contact' passHref>
-                        <Button colorScheme='blue' variant='outline'>Contact</Button>
+                        <Button colorScheme={scheme} variant='outline'>Contact</Button>
                     </NextLink>
                     <NextLink href='/services' passHref>
-                        <Button colorScheme='blue' variant='outline'>Services</Button>
+                        <Button colorScheme={scheme} variant='outline'>Services</Button>
                     </NextLink>
                     <NextLink href='/404040404040404' passHref>
-                        <Button colorScheme='blue' variant='outline'>404</Button>
+                        <Button colorScheme={scheme} variant='outline'>404</Button>
                     </NextLink>
                 </Stack>
             </nav>
